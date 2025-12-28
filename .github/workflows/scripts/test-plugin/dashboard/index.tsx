06f54ae2babc31184c8bf138ac5670894f86c5e2
rishikesh <rishi@git.com>
2025-12-28 12:46:16 +0530

    complete test case pass

--- .github/workflows/scripts/test-plugin/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import { defineDashboardExtension } from '@vendure/dashboard';
-
-defineDashboardExtension({
-    // Let's add a simple test page to check things are working
-    actionBarItems: [{
-        pageId: 'product-list',
-        component: () => <div data-testid="test-component">test component</div>
-    }]
-});
